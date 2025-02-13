4c4766c3e344dfa1717bc27bc2ba9eab39d4dc5e
後藤祐大 <gotoyudai@gotouyuudainoMacBook-Pro.local>
2025-02-13 22:22:51 +0900

    day3

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,35 @@
-// import postgres from 'postgres';
+import postgres from 'postgres';
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-// async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
+async function listInvoices() {
+ 	const data = await sql`
+     SELECT invoices.amount, customers.name
+     FROM invoices
+     JOIN customers ON invoices.customer_id = customers.id
+     WHERE invoices.amount = 666;
+   `;
 
-// 	return data;
-// }
-
+ 	return data;
+ }
+// 4. GET関数を修正
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
+  try {
+    return Response.json(await listInvoices());
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
+
+
+//export async function GET() {
+//  return Response.json({
+//    message:
+//      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+//  });
   // try {
   // 	return Response.json(await listInvoices());
   // } catch (error) {
   // 	return Response.json({ error }, { status: 500 });
   // }
-}
+//}
